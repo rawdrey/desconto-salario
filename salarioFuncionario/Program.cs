fb13ecb255b68f544afb9597f5031a9a8ab393a1
Rawdrey Nobre <rawdrey@gmail.com>
2024-03-16 16:17:20 -0300

    implementado função calcular salario líquido,entre outras melhorias

--- salarioFuncionario/Program.cs
+++ salarioFuncionario/Program.cs
@@ -10,6 +10,22 @@ namespace salarioFuncionario
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Incira o nome do funcionário");
+            string funcionario = Console.ReadLine();
+            Console.WriteLine("Incira o salário:");
+            double salarioBruto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Incira o %imposto sobre o salário");
+            double porcentagemImposto = double.Parse(Console.ReadLine());
+
+            double salarioLiquido = calcularSalarioLiquido(salarioBruto, porcentagemImposto);
+            Console.WriteLine($"O funcionário {funcionario}, recebe um salário líquido de R${salarioLiquido:F2}.");
         }
+            public static doub
+            le calcularSalarioLiquido(double salarioBruto, doubleporcentagemImposto)
+            
+            double desconto = salarioBruto * (porcentoImposto / 100);
+            salarioBrutoLiquido = salarioBruto - desconto;
+            return salarioLiquido;
+        
     }
 }
